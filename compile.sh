695e7278ea42d304c5cdb17a348fb3546362f40e
wbartel <wilhelm.bartel@streamonkey.de>
2021-10-07 10:24:03 +0200

    add cache for faster subsequent builds

--- compile.sh
+++ compile.sh
@@ -19,6 +19,11 @@ FLAGS=(
   -s USE_SDL=2                    # use SDL2
   -s USE_PTHREADS=1               # enable pthreads support
   -s INITIAL_MEMORY=33554432      # 33554432 bytes = 32 MB
+  -s INVOKE_RUN=0   
+  -s PROXY_TO_PTHREAD=1                            # not to run the main() in the beginning
+  -s EXPORTED_FUNCTIONS="[_main]" 
+  -s EXPORTED_RUNTIME_METHODS="[FS, cwrap, setValue, writeAsciiToMemory]"   # export preamble funcs
+  
   -o /dist/ffprobe-core.js
   fftools/cmdutils.c fftools/ffprobe.c
 )
