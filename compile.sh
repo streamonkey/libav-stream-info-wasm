b07e2b1b0e4b72aea006f1f296d06ebc735ba7bc
RSWilli <bartel.wilhelm@gmail.com>
2022-12-15 13:31:11 +0100

    make package

--- compile.sh
+++ compile.sh
@@ -9,13 +9,12 @@ mkdir -p /dist
   # -I. \
   # -I./fftools \
 
-  
+# https://emscripten.org/docs/tools_reference/emcc.html#emccdoc
 FLAGS=(
   --bind
-  -s MODULARIZE=1
+  # -s MODULARIZE=1
   -s EXPORT_ES6=1
   -O3 --closure 1
-  # -O0
   -I. -I./fftools
   -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Llibpostproc -Llibswscale -Llibswresample
   -Qunused-arguments
@@ -25,21 +24,19 @@ FLAGS=(
   -s INITIAL_MEMORY=33554432      
   -s INVOKE_RUN=0  
   -s EXIT_RUNTIME=1  
-  # -s PROXY_TO_PTHREAD=1
   -s EXPORTED_RUNTIME_METHODS="[FS]"   # export preamble funcs
   --pre-js /src/pre.js
   --post-js /src/post.js
   -o /dist/ffprobe-core.js
-  # fftools/cmdutils.c fftools/ffprobe.c
   /src/ffprobe-wrapper.cpp
 )
 
 emcc "${FLAGS[@]}"
 
-echo "build done, replacing strings in output" 
+# echo "build done, replacing strings in output" 
 
-#
-# fix wasm core for vitejs SSR
-#
-sed -i 's/new URL("ffprobe-core.wasm",import.meta.url)/""/g' /dist/ffprobe-core.js
-sed -i 's/new URL("ffprobe-core.worker.js",import.meta.url)/""/g' /dist/ffprobe-core.js
+# #
+# # fix wasm core for vitejs SSR
+# #
+# sed -i 's/new URL("ffprobe-core.wasm",import.meta.url)/""/g' /dist/ffprobe-core.js
+# sed -i 's/new URL("ffprobe-core.worker.js",import.meta.url)/""/g' /dist/ffprobe-core.js
